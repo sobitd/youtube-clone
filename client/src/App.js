b1e2ff8caae6eaec810cb339c9fa7ac08c36f3e7
Daniel Sobit <sobitdaniel@gmail.com>
2022-09-23 20:47:12 +0300

    add dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import SideBar from "./components/SideBar/SideBar";
+import RecommendedVideos from "./components/RecommendedVideos/RecommendedVideos";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App">
-    <h3>Soon</h3>
-  </div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/">
+            <div className="mainpage">
+              <SideBar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,5 @@
+// Remains visible on all routes
+
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawes'
