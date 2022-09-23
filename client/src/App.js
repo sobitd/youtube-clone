56951cca035be362a4540ee241da7b030a881de4
Daniel Sobit <sobitdaniel@gmail.com>
2022-09-23 22:33:30 +0300

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,21 @@ import React from "react";
 import Header from "./components/Header/Header";
 import SideBar from "./components/SideBar/SideBar";
 import RecommendedVideos from "./components/RecommendedVideos/RecommendedVideos";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-        <Switch>
+        <Routes>
           <Route path="/">
             <div className="mainpage">
               <SideBar />
               <RecommendedVideos />
             </div>
           </Route>
-        </Switch>
+        </Routes>
       </Router>
     </div>
   );
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,38 @@
 // Remains visible on all routes
 
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import VideoCallIcon from "@mui/icons-material/VideoCall";
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import { Avatar } from "@mui/material";
+import "./Header.css";
+
+
+function Header() {
+  return (
+    <div className="container">
+      <div className="menu-container">
+        <MenuIcon />
+        <Link to="/">
+          <YouTubeIcon className="youtube-logo" />
+        </Link>
+      </div>
+      <div className="search-container">
+        <input type="text" />
+        <SearchIcon className="search-btn" />
+      </div>
+      <div className="user-container">
+        <VideoCallIcon className="user-cont-icon" />
+        <AppsIcon className="user-cont-icon" />
+        <NotificationsIcon className="user-cont-icon" />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
